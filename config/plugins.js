08cb2f4498b84aaef7f89c6d6ae9bd222bfc568d
gentuuu <60502683+gentuuu@users.noreply.github.com>
2024-04-13 17:56:19 +0200

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,15 @@
-module.exports = () => ({});
+module.exports = {
+    graphql: {
+      config: {
+        endpoint: '/graphql',
+        shadowCRUD: true,
+        playgroundAlways: true,
+        depthLimit: 10,
+        amountLimit: 100,
+        apolloServer: {
+          tracing: false,
+          introspection: true,
+        },
+      },
+    },
+  };
