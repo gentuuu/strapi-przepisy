97c86d73de188313af0867c272de6dfc8938db06
gentuuu <60502683+gentuuu@users.noreply.github.com>
2024-04-14 14:53:03 +0200

    Update middlewares.js
    asd

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,30 @@
-module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+// module.exports = [
+//   'strapi::logger',
+//   'strapi::errors',
+//   'strapi::security',
+//   'strapi::cors',
+//   'strapi::poweredBy',
+//   'strapi::query',
+//   'strapi::body',
+//   'strapi::session',
+//   'strapi::favicon',
+//   'strapi::public',
+// ];
+
+
+module.exports = {
+  load: {
+    before: ['timer', 'responseTime', 'logger', 'cors', 'responses', 'gzip'],
+    order: [],
+    after: ['parser', 'router'],
+  },
+  settings: {
+    timer: {
+      enabled: true,
+    },
+    cors: {
+      enabled: true,
+      origin: ['http://localhost:5173/', 'http://gentuuu4.online/'],
+    },
+  },
+};
