7b9e6a0bfb4c8a9af8be919263cd8809a1268693
gentuuu <60502683+gentuuu@users.noreply.github.com>
2024-04-14 13:52:31 +0200

    Update server.js
    gfh

--- config/server.js
+++ config/server.js
@@ -1,17 +1,12 @@
-// module.exports = ({ env }) => ({
-//   host: env('HOST', '0.0.0.0'),
-//   port: env.int('PORT', 1337),
-//   app: {
-//     keys: env.array('APP_KEYS'),
-//   },
-//   webhooks: {
-//     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-//   },
-// });
-
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET'),
@@ -25,3 +20,4 @@ module.exports = ({ env }) => ({
     origin: ['http://localhost:5173/'], // replace with your frontend's origin
   },
 });
+
