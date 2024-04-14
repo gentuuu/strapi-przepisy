6ca817536cc183a9216fd4f7b3bf34dda38f4b20
gentuuu <60502683+gentuuu@users.noreply.github.com>
2024-04-14 13:16:48 +0200

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,11 +1,31 @@
+// module.exports = ({ env }) => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 1337),
+//   app: {
+//     keys: env.array('APP_KEYS'),
+//   },
+//   webhooks: {
+//     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+//   },
+// });
+
+const cors = require('cors');
+
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
+
+  server: {
+    middlewares: [
+
+      cors({
+        origin: ['http://localhost:3000', 'https://twoj-adres-url.herokuapp.com'], // Dodaj adresy URL, z których chcesz zezwolić na dostęp do zasobów
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'HEAD'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+        credentials: true,
+      }),
+
+    ],
   },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
+
 });
+
 
