f5de00dcc1241c719102cea78009d992e6fdd7db
gentuuu <60502683+gentuuu@users.noreply.github.com>
2024-04-23 17:58:34 +0200

    sa
    dasd

--- config/plugins.js
+++ config/plugins.js
@@ -1,17 +1,17 @@
 module.exports = ({ env }) => ({
-    graphql: {
-      config: {
-        endpoint: '/graphql',
-        shadowCRUD: true,
-        playgroundAlways: true,
-        depthLimit: 10,
-        amountLimit: 100,
-        apolloServer: {
-          tracing: false,
-          introspection: true,
-        },
-      },
-    },
+    // graphql: {
+    //   config: {
+    //     endpoint: '/graphql',
+    //     shadowCRUD: true,
+    //     playgroundAlways: true,
+    //     depthLimit: 10,
+    //     amountLimit: 100,
+    //     apolloServer: {
+    //       tracing: false,
+    //       introspection: true,
+    //     },
+    //   },
+    // },
 
     upload: {
       config: {
