7d73266a0a754d058f58a6f1219a7a9ab83a4594
gentuuu <60502683+gentuuu@users.noreply.github.com>
2024-04-23 18:02:28 +0200

    we
    qweq

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,20 +1,7 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
+  'strapi::security',
   {
     name: 'strapi::cors',
     config: {
@@ -34,4 +21,3 @@ module.exports = [
   'strapi::favicon',
   'strapi::public',
 ];
-
--- config/plugins.js
+++ config/plugins.js
@@ -1,31 +1,35 @@
-module.exports = ({ env }) => ({
-    // graphql: {
-    //   config: {
-    //     endpoint: '/graphql',
-    //     shadowCRUD: true,
-    //     playgroundAlways: true,
-    //     depthLimit: 10,
-    //     amountLimit: 100,
-    //     apolloServer: {
-    //       tracing: false,
-    //       introspection: true,
-    //     },
-    //   },
-    // },
+// module.exports = {
+//     graphql: {
+//       config: {
+//         endpoint: '/graphql',
+//         shadowCRUD: true,
+//         playgroundAlways: true,
+//         depthLimit: 10,
+//         amountLimit: 100,
+//         apolloServer: {
+//           tracing: false,
+//           introspection: true,
+//         },
+//       },
+//     },
+//   };
 
-    upload: {
-      config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
-        },
-        actionOptions: {
-          upload: {},
-          delete: {},
-        },
+
+module.exports = ({ env }) => ({
+
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
       },
     },
-});
+  },
 
+});
